f9c9c8761bbe74623cd0094485eb3efec51940a0
Scott Hutchings <scott_hutchings@byu.edu>
2024-09-17 15:04:12 -0600

    basic html template

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"byu.edu/poc-imaging/pages"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", pages.IndexHtmlAdapter)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
